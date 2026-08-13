a457236639e6136a2ec28a5f0e773ccc0bc4fa9a
mahee96 <47920326+mahee96@users.noreply.github.com>
2026-08-14 03:35:49 +0530

    fix: appids should show cancel when in select mode despite showing delete

--- AltStore/App IDs/AppIDsViewController.swift
+++ AltStore/App IDs/AppIDsViewController.swift
@@ -209,23 +209,25 @@ private extension AppIDsViewController
                     let title = selectedCount > 0 ? NSLocalizedString("Delete", comment: "") : NSLocalizedString("Cancel", comment: "")
                     let style: UIBarButtonItem.Style = selectedCount > 0 ? .done : .plain
                     self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: title, style: style, target: self, action: #selector(self.editButtonTapped))
+                    
+                    if selectedCount > 0
+                    {
+                        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: NSLocalizedString("Cancel", comment: ""), style: .plain, target: self, action: #selector(self.cancelButtonTapped))
+                    }
+                    else
+                    {
+                        self.navigationItem.rightBarButtonItem = nil
+                    }
                 }
                 else
                 {
                     self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: NSLocalizedString("Edit", comment: ""), style: .plain, target: self, action: #selector(self.editButtonTapped))
+                    self.navigationItem.rightBarButtonItem = self.doneBarButtonItem
                 }
             }
             else
             {
                 self.navigationItem.leftBarButtonItem = nil
-            }
-            
-            if self.isEditingMode
-            {
-                self.navigationItem.rightBarButtonItem = nil
-            }
-            else
-            {
                 self.navigationItem.rightBarButtonItem = self.doneBarButtonItem
             }
             
@@ -389,11 +391,31 @@ private extension AppIDsViewController
     
     func updateLeftBarButtonItem()
     {
+        self.updateBarButtonItems()
+    }
+    
+    func updateBarButtonItems()
+    {
+        guard self.isEditingMode else { return }
         let selectedCount = self.collectionView.indexPathsForSelectedItems?.count ?? 0
         let title = selectedCount > 0 ? NSLocalizedString("Delete", comment: "") : NSLocalizedString("Cancel", comment: "")
         let style: UIBarButtonItem.Style = selectedCount > 0 ? .done : .plain
         self.navigationItem.leftBarButtonItem?.title = title
         self.navigationItem.leftBarButtonItem?.style = style
+        
+        if selectedCount > 0
+        {
+            self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: NSLocalizedString("Cancel", comment: ""), style: .plain, target: self, action: #selector(self.cancelButtonTapped))
+        }
+        else
+        {
+            self.navigationItem.rightBarButtonItem = nil
+        }
+    }
+    
+    @objc func cancelButtonTapped()
+    {
+        self.exitEditMode()
     }
     
     @objc func editButtonTapped()
@@ -600,7 +622,7 @@ extension AppIDsViewController
     {
         if self.isEditingMode
         {
-            self.updateLeftBarButtonItem()
+            self.updateBarButtonItems()
             if let cell = collectionView.cellForItem(at: indexPath) as? AppBannerCollectionViewCell {
                 cell.setEditing(true, isSelected: true, animated: true)
             }
@@ -611,7 +633,7 @@ extension AppIDsViewController
     {
         if self.isEditingMode
         {
-            self.updateLeftBarButtonItem()
+            self.updateBarButtonItems()
             if let cell = collectionView.cellForItem(at: indexPath) as? AppBannerCollectionViewCell {
                 cell.setEditing(true, isSelected: false, animated: true)
             }
