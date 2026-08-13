55428002c9178e21defc7aa98a9dd6130393b9f0
mahee96 <47920326+mahee96@users.noreply.github.com>
2026-08-13 10:42:46 +0530

    cleanup: prepare for moving altstorecore into altStore (keep shared parts separate)

--- Shared/WidgetData/WidgetDataManager.swift
+++ Shared/WidgetData/WidgetDataManager.swift
@@ -1,43 +1,13 @@
 //
 //  WidgetDataManager.swift
-//  AltStoreCore
+//  SideStore
 //
-//  Created by Magesh K on 8/8/26.
+//  Created by Magesh K on 8/13/26.
 //  Copyright © 2026 SideStore. All rights reserved.
 //
 
-@preconcurrency import UIKit
+import UIKit
 import WidgetKit
-import CoreData
-@preconcurrency import AltSign
-
-public struct WidgetAppItem: Codable, Sendable {
-    public var name: String
-    public var bundleIdentifier: String
-    public var expirationDate: Date
-    public var refreshedDate: Date
-    public var tintColorHex: String?
-
-    public init(name: String, bundleIdentifier: String, expirationDate: Date, refreshedDate: Date, tintColorHex: String?) {
-        self.name = name
-        self.bundleIdentifier = bundleIdentifier
-        self.expirationDate = expirationDate
-        self.refreshedDate = refreshedDate
-        self.tintColorHex = tintColorHex
-    }
-}
-
-public struct WidgetDataSnapshot: Codable, Sendable {
-    public var activeApps: [WidgetAppItem]
-    public var allApps: [WidgetAppItem]
-    public var lastUpdated: Date
-
-    public init(activeApps: [WidgetAppItem] = [], allApps: [WidgetAppItem] = [], lastUpdated: Date = Date()) {
-        self.activeApps = activeApps
-        self.allApps = allApps
-        self.lastUpdated = lastUpdated
-    }
-}
 
 public final class WidgetDataManager: @unchecked Sendable {
     public static let shared = WidgetDataManager()
@@ -111,53 +81,6 @@ public final class WidgetDataManager: @unchecked Sendable {
             try data.write(to: url, options: .atomic)
         } catch {
             debugLog("[WidgetDataManager] Failed to write snapshot JSON: \(error)")
-        }
-    }
-}
-
-extension WidgetDataManager {
-    public static func publishCurrentInstalledApps(in context: NSManagedObjectContext) async {
-        await context.performAsync {
-            let activeFetch = InstalledApp.activeAppsFetchRequest()
-            activeFetch.returnsObjectsAsFaults = false
-            let activeApps = (try? context.fetch(activeFetch)) ?? []
-
-            let allFetch = InstalledApp.fetchRequest()
-            allFetch.returnsObjectsAsFaults = false
-            let allApps = (try? context.fetch(allFetch)) ?? []
-
-            let sortedActiveApps = activeApps.sorted { $0.name < $1.name }
-            let sortedAllApps = allApps.sorted { $0.name < $1.name }
-
-            var icons: [String: UIImage] = [:]
-            for app in sortedAllApps {
-                if let application = ALTApplication(fileURL: app.fileURL),
-                   let icon = application.icon {
-                    icons[app.bundleIdentifier] = icon
-                }
-            }
-
-            let activeItems = sortedActiveApps.map { app in
-                WidgetAppItem(
-                    name: app.name,
-                    bundleIdentifier: app.bundleIdentifier,
-                    expirationDate: app.expirationDate,
-                    refreshedDate: app.refreshedDate,
-                    tintColorHex: app.storeApp?.tintColor?.hexString
-                )
-            }
-
-            let allItems = sortedAllApps.map { app in
-                WidgetAppItem(
-                    name: app.name,
-                    bundleIdentifier: app.bundleIdentifier,
-                    expirationDate: app.expirationDate,
-                    refreshedDate: app.refreshedDate,
-                    tintColorHex: app.storeApp?.tintColor?.hexString
-                )
-            }
-
-            WidgetDataManager.shared.publishWidgetData(activeItems: activeItems, allItems: allItems, icons: icons)
         }
     }
 }
